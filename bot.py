2636435b77bfbacfa1844309bd4856347c31195b
ozodbek2322 <124016731+ozodbek2322@users.noreply.github.com>
2026-02-22 20:23:27 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,29 @@
 import telebot
 import threading
-import time
-import requests
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 TOKEN = "8416702430:AAF5lNyiteQ97-rqpK4lU95F-u9AJfKUzO4"
 ADMIN_ID = 8348539088
 
 bot = telebot.TeleBot(TOKEN)
 
-# Forward
 @bot.message_handler(content_types=['text','photo','video','document','audio','voice'])
 def forward_all(message):
     bot.forward_message(ADMIN_ID, message.chat.id, message.message_id)
 
-# Anti-sleep funksiyasi
-def keep_alive():
-    while True:
-        try:
-            requests.get("https://google.com")
-        except:
-            pass
-        time.sleep(300)
+# Web server (Render uxlamasligi uchun)
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Bot is alive")
 
-threading.Thread(target=keep_alive).start()
+def run_web():
+    server = HTTPServer(('0.0.0.0', 10000), Handler)
+    server.serve_forever()
+
+threading.Thread(target=run_web).start()
 
 print("Bot ishlayapti...")
 bot.infinity_polling()
-
-
